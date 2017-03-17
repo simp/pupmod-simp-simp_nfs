83590a94e93f1dfb67dae7e6381ebb12ff4a7983
Nick Miller <nick.miller@onyxpoint.com>
2017-03-17 08:40:37 -0400

    (SIMP-2823) Fix acceptance test
    Our recent changes to simp/simp caused this test to misbehave, citing
    missing dependencies among other things. It now passes without issue.
    
    SIMP-2823 #close

--- spec/acceptance/suites/default/00_default_spec.rb
+++ spec/acceptance/suites/default/00_default_spec.rb
@@ -37,89 +37,72 @@ describe 'simp_nfs stock classes' do
       domains = _domains.join(',')
 
       manifest = <<-EOM
-hiera_include("classes")
-EOM
+        include 'simp_options'
+        include 'pam::access'
+        include 'sudo'
+        include 'ssh'
+        include 'simp::nsswitch'
+        include 'simp_openldap::client'
+        include 'simp::sssd::client'
+        include 'simp_nfs'
+      EOM
 
       hieradata = <<-EOM
 ---
-# Turn this off because we don't have a remote server
-simp_options::rsync: false
-simp_options::ldap: true
+
+simp_nfs::home_dir_server: #{nfs_server}
+nfs::client::stunnel::nfs_server: #{nfs_server}
+
+# Options
+# Use fallback ciphers/macs to ensure ssh capability on any platform
+ssh::server::conf::ciphers:
+- 'aes256-cbc'
+- 'aes192-cbc'
+- 'aes128-cbc'
+ssh::server::conf::macs:
+- 'hmac-sha1'
+simp_options::clamav: false
+simp_options::dns::servers: ['8.8.8.8']
+simp_options::puppet::server: #{server}
+simp_options::puppet::ca: #{server}
+simp::yum::servers: ['#{server}']
+simp_options::ntpd::servers: ['time.nist.gov']
 simp_options::sssd: true
 simp_options::stunnel: true
 simp_options::tcpwrappers: true
+simp_options::pam: true
 simp_options::firewall: true
 simp_options::pki: true
 simp_options::pki::source: '/etc/pki/simp-testing/pki'
-simp_options::pki::private_key_source: "file://%{hiera('pki_dir')}/private/%{::fqdn}.pem"
-simp_options::pki::public_key_source: "file://%{hiera('pki_dir')}/public/%{::fqdn}.pub"
-simp_options::pki::cacerts_sources:
-  - "file://%{hiera('pki_dir')}/cacerts"
-
-simp_options::trusted_nets:
- - 'ALL'
-
-simp_options::ldap::uri:
- - 'ldap://#{nfs_server}'
-simp_options::ldap::base_dn: '#{domains}'
-simp_options::ldap::bind_dn: 'cn=hostAuth,ou=Hosts,#{domains}'
+simp_options::trusted_nets: ['ALL']
+simp_options::ldap: true
 simp_options::ldap::bind_pw: 'foobarbaz'
 simp_options::ldap::bind_hash: '{SSHA}BNPDR0qqE6HyLTSlg13T0e/+yZnSgYQz'
-simp_options::ldap::sync_dn: 'cn=LDAPSync,ou=Hosts,#{domains}'
 simp_options::ldap::sync_pw: 'foobarbaz'
 simp_options::ldap::sync_hash: '{SSHA}BNPDR0qqE6HyLTSlg13T0e/+yZnSgYQz'
-simp_options::ldap::root_dn: 'cn=LDAPAdmin,ou=People,#{domains}'
-simp_options::ldap::root_hash: '{SSHA}BNPDR0qqE6HyLTSlg13T0e/+yZnSgYQz'
-simp_options::ldap::master: 'ldap://#{nfs_server}'
+# simp_openldap::server::conf::rootpw: '{SSHA}BNPDR0qqE6HyLTSlg13T0e/+yZnSgYQz'
 # suP3rP@ssw0r!
-simp_options::ldap::root_hash: "{SSHA}ZcqPNbcqQhDNF5jYTLGl+KAGcrHNW9oo"
-
+simp_openldap::server::conf::rootpw: "{SSHA}ZcqPNbcqQhDNF5jYTLGl+KAGcrHNW9oo"
 sssd::domains:
- - 'LDAP'
+  - LDAP
+simp::is_mail_server: false
+pam::wheel_group: 'administrators'
 
+# Settings to make beaker happy
 pam::access::users:
   defaults:
     origins:
       - ALL
     permission: '+'
   vagrant:
-
 sudo::user_specifications:
   vagrant_all:
     user_list: ['vagrant']
     cmnd: ['ALL']
     passwd: false
-
-pam::wheel_group: 'administrators'
-
 ssh::server::conf::permitrootlogin: true
-ssh::server::conf::authorizedkeysfile: ".ssh/authorized_keys"
-
-# Use fallback ciphers/macs to ensure ssh capability on any platform
-ssh::server::conf::ciphers:
- - 'aes256-cbc'
- - 'aes192-cbc'
- - 'aes128-cbc'
-ssh::server::conf::macs:
- - 'hmac-sha1'
-
-# For testing
-simp::is_mail_server: false
-
-simp_nfs::home_dir_server: #{nfs_server}
-
-nfs::client::stunnel::nfs_server: "#{nfs_server}"
-
-classes:
-  - "pam::access"
-  - "pam::wheel"
-  - "simp"
-  - "simp::admin"
-  - "simp_nfs"
-  - "simp::nsswitch"
-  - "ssh"
-  - "tcpwrappers"
-EOM
+ssh::server::conf::authorizedkeysfile: .ssh/authorized_keys
+      EOM
 
       test_user_ldif = <<-EOM
 dn: cn=test.user,ou=Group,#{domains}
@@ -152,58 +135,53 @@ homeDirectory: /home/test.user
 #suP3rP@ssw0r!
 userPassword: {SSHA}r2GaizHFWY8pcHpIClU0ye7vsO4uHv/y
 pwdReset: TRUE
-EOM
+      EOM
 
       test_group_ldif = <<-EOM
 dn: cn=administrators,ou=Group,#{domains}
 changetype: modify
 add: memberUid
 memberUid: test.user
-EOM
+      EOM
 
       if servers.include?(node)
         it 'should install nfs, openldap, and create test.user' do
           # Construct server hieradata; export home directories.
-          server_hieradata = hieradata + <<-EOM
-
-nfs::is_server: true
-simp_nfs::export_home::create_home_dirs: true
+          server_hieradata = hieradata + <<-EOM.gsub(/^\s+/,'')
+            nfs::is_server: true
+            simp_nfs::export_home::create_home_dirs: true
           EOM
 
           server_manifest = manifest + <<-EOM
-
-include 'simp_nfs::export::home'
-include 'simp::server::ldap'
+            include 'simp_nfs::export::home'
+            include 'simp::server::ldap'
           EOM
 
           # Apply
           set_hieradata_on(node, server_hieradata, 'default')
-          on(node, ('mkdir -p /usr/local/sbin/simp'))
+          on(node, 'mkdir -p /usr/local/sbin/simp')
+          apply_manifest_on(node, server_manifest, :catch_failures => true)
           apply_manifest_on(node, server_manifest, :catch_failures => true)
 
           # Create test.user
-          on(node, "cat <<EOF > /root/user_ldif.ldif
-#{test_user_ldif}
-EOF")
-
-          on(node, "cat <<EOF > /root/group_ldif.ldif
-#{test_group_ldif}
-EOF")
+          create_remote_file(node, '/root/user_ldif.ldif', test_user_ldif)
+          create_remote_file(node, '/root/group_ldif.ldif', test_group_ldif)
 
           # Create test.user and add to administrators
           on(node, "ldapadd -D cn=LDAPAdmin,ou=People,#{domains} -H ldap://#{nfs_server} -w suP3rP@ssw0r! -x -Z -f /root/user_ldif.ldif")
           on(node, "ldapmodify -D cn=LDAPAdmin,ou=People,#{domains} -H ldap://#{nfs_server} -w suP3rP@ssw0r! -x -Z -f /root/group_ldif.ldif")
 
           # Ensure the cache is built, don't wait for enum timeout
-          on(node, "service sssd restart")
+          require 'pry';binding.pry
+          on(node, 'service sssd restart')
 
-          user_info = on(node, "id test.user", :acceptable_exit_codes => [0])
+          user_info = on(node, 'id test.user', :acceptable_exit_codes => [0])
           expect(user_info.stdout).to match(/.*uid=10000\(test.user\).*gid=10000\(test.user\)/)
         end
       else
         it "should set up #{node}" do
           set_hieradata_on(node, hieradata, 'default')
-          on(node, ('mkdir -p /usr/local/sbin/simp'))
+          on(node, 'mkdir -p /usr/local/sbin/simp')
           apply_manifest_on(node, manifest, :catch_failures => true)
         end
       end
@@ -212,7 +190,7 @@ EOF")
     it 'should create the test.user home directory mount on the servers using the cron job' do
       servers.each do |node|
         # Create test.user's homedir via cron, and ensure it gets mounted
-        on(node, "/etc/cron.hourly/create_home_directories.rb")
+        on(node, '/etc/cron.hourly/create_home_directories.rb')
         on(node, 'ls /var/nfs/home/test.user')
         on(node, "runuser -l test.user -c 'touch ~/testfile'")
         mount = on(node, "mount")
@@ -222,7 +200,7 @@ EOF")
 
     it 'should have file propagation to the clients' do
       clients.each do |node|
-        on(node, "ls /home/test.user/testfile", :acceptable_exit_codes => [0])
+        on(node, 'ls /home/test.user/testfile', :acceptable_exit_codes => [0])
       end
     end
   end
