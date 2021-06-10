5cf952e3e9c1c05d6a3ebf50b56922e9ba698699
Jeanne Greulich <jeanne.greulich@onyxpoint.com>
2021-06-10 16:59:14 -0400

    * update create_home_directories to set tls params.   EL8 in FIPS mode could not talk to LDAP server on   EL7 because ciphers did not match.  The script was using   the default policies which only have TLS1_3 in them.   EL7 uses an older version of openssl which does not have   tls1_3.  So had to set the ciphers in the script.
    remove puppet5 because of concat issues
    fix typo in template

--- spec/classes/create_home_dirs_spec.rb
+++ spec/classes/create_home_dirs_spec.rb
@@ -23,8 +23,8 @@ describe 'simp_nfs::create_home_dirs' do
           }}
           it_behaves_like "a structured module"
           if ['RedHat','CentOS'].include?(facts[:os][:name])
-            it { is_expected.to create_file('/etc/cron.hourly/create_home_directories.rb').with_content(%r(self\.ciphers = '.*256.*)) }
-            it { is_expected.to create_file('/etc/cron.hourly/create_home_directories.rb').with_content(%r(self\.ciphers = '.*128.*)) }
+            it { is_expected.to create_file('/etc/cron.hourly/create_home_directories.rb').with_content(%r(ciphers_list = '.*256.*)) }
+            it { is_expected.to create_file('/etc/cron.hourly/create_home_directories.rb').with_content(%r(ciphers_list = '.*128.*)) }
           end
         end
         context 'with multiple URIs' do
