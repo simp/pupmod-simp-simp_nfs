eaffa1a17450b7c9fcf5367ba9602cf46e4755c7
Steven Pritchard <steve@sicura.us>
2024-12-16 20:25:44 -0600

    Clean up for rubocop

--- spec/acceptance/suites/default/00_setup_ldap_spec.rb
+++ spec/acceptance/suites/default/00_setup_ldap_spec.rb
@@ -4,98 +4,94 @@ test_name 'Set up ldap server '
 
 describe 'simp_nfs stock classes' do
   stunnel_setting = true
-  ldap_server = only_host_with_role(hosts,'ldap')
+  ldap_server = only_host_with_role(hosts, 'ldap')
   ldap_server_fqdn = fact_on(ldap_server, 'fqdn')
 
-  _domains = fact_on(ldap_server, 'domain').split('.')
-  _domains.map! { |d|
+  domain_list = fact_on(ldap_server, 'domain').split('.')
+  domain_list.map! do |d|
     "dc=#{d}"
-  }
-  domains = _domains.join(',')
+  end
+  domains = domain_list.join(',')
 
   common_hieradata = File.read(File.expand_path('files/common_hieradata.yaml.erb', File.dirname(__FILE__)))
 
   context 'setup ldap server ' do
-
     let(:ldap_type)        { 'plain' }
-    let(:server_hieradata) { File.read(File.expand_path("files/#{ldap_type}/server_hieradata.yaml.erb", File.dirname(__FILE__)))}
-    let (:hieradata){ "#{common_hieradata}" + "\n#{server_hieradata}"}
+    let(:server_hieradata) { File.read(File.expand_path("files/#{ldap_type}/server_hieradata.yaml.erb", File.dirname(__FILE__))) }
+    let(:hieradata) { common_hieradata.to_s + "\n#{server_hieradata}" }
 
+    test_user_ldif = <<~EOM
+      dn: cn=test.user,ou=Group,#{domains}
+      objectClass: posixGroup
+      objectClass: top
+      cn: test.user
+      gidNumber: 10000
+      description: 'Test user'
 
-    test_user_ldif = <<-EOM
-dn: cn=test.user,ou=Group,#{domains}
-objectClass: posixGroup
-objectClass: top
-cn: test.user
-gidNumber: 10000
-description: 'Test user'
+      dn: cn=monster.user,ou=Group,#{domains}
+      objectClass: posixGroup
+      objectClass: top
+      cn: monster.user
+      gidNumber: 11000
+      description: 'Monster user'
 
-dn: cn=monster.user,ou=Group,#{domains}
-objectClass: posixGroup
-objectClass: top
-cn: monster.user
-gidNumber: 11000
-description: 'Monster user'
+      dn: uid=test.user,ou=People,#{domains}
+      uid: test.user
+      cn: test.user
+      givenName: Test
+      sn: User
+      mail: test.user@funurl.net
+      objectClass: inetOrgPerson
+      objectClass: posixAccount
+      objectClass: top
+      objectClass: shadowAccount
+      objectClass: ldapPublicKey
+      shadowMax: 180
+      shadowMin: 1
+      shadowWarning: 7
+      shadowLastChange: 10701
+      loginShell: /bin/bash
+      uidNumber: 10000
+      gidNumber: 10000
+      homeDirectory: /mnt/test.user
+      # suP3rP@ssw0r!
+      userPassword: {SSHA}yOdnVOQYXOEc0Gjv4RRY5BnnFfIKLI3/
+      pwdReset: TRUE
 
-dn: uid=test.user,ou=People,#{domains}
-uid: test.user
-cn: test.user
-givenName: Test
-sn: User
-mail: test.user@funurl.net
-objectClass: inetOrgPerson
-objectClass: posixAccount
-objectClass: top
-objectClass: shadowAccount
-objectClass: ldapPublicKey
-shadowMax: 180
-shadowMin: 1
-shadowWarning: 7
-shadowLastChange: 10701
-loginShell: /bin/bash
-uidNumber: 10000
-gidNumber: 10000
-homeDirectory: /mnt/test.user
-# suP3rP@ssw0r!
-userPassword: {SSHA}yOdnVOQYXOEc0Gjv4RRY5BnnFfIKLI3/
-pwdReset: TRUE
-
-dn: uid=monster.user,ou=People,#{domains}
-uid: monster.user
-cn: monster.user
-givenName: monster
-sn: User
-mail: monster.user@funurl.net
-objectClass: inetOrgPerson
-objectClass: posixAccount
-objectClass: top
-objectClass: shadowAccount
-objectClass: ldapPublicKey
-shadowMax: 180
-shadowMin: 1
-shadowWarning: 7
-shadowLastChange: 10701
-loginShell: /bin/bash
-uidNumber: 11000
-gidNumber: 11000
-homeDirectory: /mnt1/monster.user
-# suP3rP@ssw0r!
-userPassword: {SSHA}yOdnVOQYXOEc0Gjv4RRY5BnnFfIKLI3/
-pwdReset: TRUE
+      dn: uid=monster.user,ou=People,#{domains}
+      uid: monster.user
+      cn: monster.user
+      givenName: monster
+      sn: User
+      mail: monster.user@funurl.net
+      objectClass: inetOrgPerson
+      objectClass: posixAccount
+      objectClass: top
+      objectClass: shadowAccount
+      objectClass: ldapPublicKey
+      shadowMax: 180
+      shadowMin: 1
+      shadowWarning: 7
+      shadowLastChange: 10701
+      loginShell: /bin/bash
+      uidNumber: 11000
+      gidNumber: 11000
+      homeDirectory: /mnt1/monster.user
+      # suP3rP@ssw0r!
+      userPassword: {SSHA}yOdnVOQYXOEc0Gjv4RRY5BnnFfIKLI3/
+      pwdReset: TRUE
     EOM
 
-    test_group_ldif = <<-EOM
-dn: cn=administrators,ou=Group,#{domains}
-changetype: modify
-add: memberUid
-memberUid: test.user
-memberUid: monster.user
+    test_group_ldif = <<~EOM
+      dn: cn=administrators,ou=Group,#{domains}
+      changetype: modify
+      add: memberUid
+      memberUid: test.user
+      memberUid: monster.user
     EOM
 
-    it 'should install, openldap, and create test.user' do
-
-
-      server_manifest = <<-EOM
+    it 'install,s openldap, and create test.user' do
+      server_manifest = <<~EOM
         include 'simp_options'
         include 'pam::access'
         include 'sudo'
@@ -123,10 +119,10 @@ memberUid: monster.user
       # Ensure the cache is built, don't wait for enum timeout
       on(ldap_server, 'service sssd restart')
 
-      user_info = on(ldap_server, 'id test.user', :acceptable_exit_codes => [0])
-      expect(user_info.stdout).to match(/.*uid=10000\(test.user\).*gid=10000\(test.user\)/)
-      monster_info = on(ldap_server, 'id monster.user', :acceptable_exit_codes => [0])
-      expect(monster_info.stdout).to match(/.*uid=11000\(monster.user\).*gid=11000\(monster.user\)/)
+      user_info = on(ldap_server, 'id test.user', acceptable_exit_codes: [0])
+      expect(user_info.stdout).to match(%r{.*uid=10000\(test.user\).*gid=10000\(test.user\)})
+      monster_info = on(ldap_server, 'id monster.user', acceptable_exit_codes: [0])
+      expect(monster_info.stdout).to match(%r{.*uid=11000\(monster.user\).*gid=11000\(monster.user\)})
     end
   end
 end
--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -15,7 +15,6 @@ unless ENV['BEAKER_provision'] == 'no'
   end
 end
 
-
 RSpec.configure do |c|
   # ensure that environment OS is ready on each host
   fix_errata_on hosts
@@ -29,22 +28,18 @@ RSpec.configure do |c|
 
   # Configure all nodes in nodeset
   c.before :suite do
-    begin
-      # Install modules and dependencies from spec/fixtures/modules
-      copy_fixture_modules_to( hosts )
+    # Install modules and dependencies from spec/fixtures/modules
+    copy_fixture_modules_to(hosts)
 
-      # Generate and install PKI certificates on each SUT
-      Dir.mktmpdir do |cert_dir|
-        run_fake_pki_ca_on( default, hosts, cert_dir )
-        hosts.each{ |sut| copy_pki_to( sut, cert_dir, '/etc/pki/simp-testing' )}
-      end
-    rescue StandardError, ScriptError => e
-      if ENV['PRY']
-        require 'pry'; binding.pry
-      else
-        raise e
-      end
+    # Generate and install PKI certificates on each SUT
+    Dir.mktmpdir do |cert_dir|
+      run_fake_pki_ca_on(default, hosts, cert_dir)
+      hosts.each { |sut| copy_pki_to(sut, cert_dir, '/etc/pki/simp-testing') }
     end
+  rescue StandardError, ScriptError => e
+    raise e unless ENV['PRY']
+    require 'pry'
+    binding.pry # rubocop:disable Lint/Debugger
   end
 end
 
@@ -54,10 +49,10 @@ def get_private_network_interface(host)
   # remove interfaces we know are not the private network interface
   interfaces.delete_if do |ifc|
     ifc == 'lo' or
-    ifc.include?('ip_') or # IPsec tunnel
-    ifc == 'enp0s3' or     # public interface for puppetlabs/centos-7.2-64-nocm virtual box
-    ifc == 'eth0'          # public interface for centos/7 virtual box
+      ifc.include?('ip_') or # IPsec tunnel
+      ifc == 'enp0s3' or     # public interface for puppetlabs/centos-7.2-64-nocm virtual box
+      ifc == 'eth0'          # public interface for centos/7 virtual box
   end
-  fail("Could not determine the interface for the #{host}'s private network") unless interfaces.size == 1
+  raise("Could not determine the interface for the #{host}'s private network") unless interfaces.size == 1
   interfaces[0]
 end
